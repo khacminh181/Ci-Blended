e3e0b871dea3dcc3e8ea3ad1e871c28012945b20
Minh Nguyen <nguyenkhacminh181@gmail.com>
2018-09-12 10:19:44 +0700

    Load player image

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 
 public class GameCanvas extends JPanel {
     Image background;
+    Image player;
 
     public GameCanvas() {
         try {
             background = ImageIO.read(new File("images/background/background.png"));
+            player = ImageIO.read(new File("images/player/MB-69/player1.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -18,5 +20,6 @@ public class GameCanvas extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         g.drawImage(background,0,0,null);
+        g.drawImage(player,0, 0, null);
     }
 }
