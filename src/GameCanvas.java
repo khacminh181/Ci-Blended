0b8a1ae4fa16f726413978e6f6c6436555f3d742
Minh Nguyen <nguyenkhacminh181@gmail.com>
2018-12-19 13:37:33 +0700

    Session5

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -2,51 +2,135 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GameCanvas extends JPanel {
     Image background;
     Image player;
 
+    ArrayList<PlayerBullet> bs;
+
+    int x = 300 - 32;
+    int y = 650 -40;
+
+    BufferedImage backBuffer;
+    Graphics backBufferGraphics;
+
+    boolean rightPressed = false;
+    boolean leftPressed = false;
+    boolean upPressed = false;
+    boolean downPressed = false;
+    boolean xPressed = false;
+
     public GameCanvas() {
+        bs = new ArrayList<>();
+
         try {
             background = ImageIO.read(new File("images/background/background.png"));
             player = ImageIO.read(new File("images/player/MB-69/player1.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        backBuffer = new BufferedImage(600, 800, BufferedImage.TYPE_INT_ARGB);
+        backBufferGraphics = backBuffer.getGraphics();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.drawImage(background,0,0,null);
-        g.drawImage(player,x, y, null);
+        g.drawImage(backBuffer, 0, 0, null);
     }
 
-    int x = 300 - 32;
-    int y = 650 -40;
-
-    public void keyPresses(KeyEvent e) {
+    void keyPressed (KeyEvent e) {
         if (e.getKeyCode() ==  KeyEvent.VK_RIGHT) {
-            x += 5;
-            this.repaint();
+            rightPressed = true;
         }
         else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            x -= 5;
-            this.repaint();
+            leftPressed = true;
         }
         else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            y += 5;
-            this.repaint();
+            downPressed = true;
         }
         else if (e.getKeyCode() == KeyEvent.VK_UP) {
-            y -= 5;
-            this.repaint();
+            upPressed = true;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_X) {
+            xPressed = true;
         }
     }
 
-    public void keyReleased(KeyEvent e) {
-        System.out.println("Canvas handling key released");
+    void keyReleased (KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            rightPressed = false;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            leftPressed = false;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            downPressed = false;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            upPressed = false;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_X) {
+            xPressed = false;
+        }
+    }
+
+    void run() {
+        if (rightPressed) {
+            x += 5;
+        }
+        if (leftPressed) {
+            x -= 5;
+        }
+        if (downPressed) {
+            y += 5;
+        }
+        if (upPressed) {
+            y -= 5;
+        }
+        for (PlayerBullet b : bs) {
+            b.y -= 2;
+        }
+
+        if (xPressed && !shootDisabled) {
+            PlayerBullet newBullet = new PlayerBullet();
+            newBullet.x = x;
+            newBullet.y = y;
+            try {
+                newBullet.image = ImageIO.read(new File("images/bullet/player/mb69bullet1.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bs.add(newBullet);
+            shootDisabled = true;
+        }
+
+        if (shootDisabled) {
+            count++;
+            if (count > 30) {
+                shootDisabled = false;
+                count = 0;
+            }
+        }
+    }
+
+    boolean shootDisabled = false;
+    int count;
+
+    void render() {
+        backBufferGraphics.drawImage(background,0,0,null);
+        backBufferGraphics.drawImage(player,x, y, null);
+        for (PlayerBullet b : bs) {
+            backBufferGraphics.drawImage(b.image,b.x,b.y, null);
+        }
+
+        this.repaint();
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -8,9 +8,7 @@ public class GameWindow extends JFrame {
     GameCanvas canvas;
 
     public GameWindow() {
-
         //Setup Window
-
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -20,7 +18,7 @@ public class GameWindow extends JFrame {
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                canvas.keyPresses(e);
+                canvas.keyPressed(e);
             }
 
             @Override
@@ -28,17 +26,26 @@ public class GameWindow extends JFrame {
                 canvas.keyReleased(e);
             }
         });
-
         this.setSize(600, 800);
         this.setResizable(false);
         this.setTitle("Title");
 
-
         //Setup Canvas
-
         canvas = new GameCanvas();
         this.setContentPane(canvas);
         this.setVisible(true);
+    }
 
+    long lastTimeRender = 0;
+
+    void gameLoop() {
+        while(true) {
+            long currentTime = System.nanoTime();
+            if (currentTime - lastTimeRender >= 17_000_000) {
+                canvas.run();
+                canvas.render();
+                lastTimeRender = currentTime;
+            }
+        }
     }
 }
